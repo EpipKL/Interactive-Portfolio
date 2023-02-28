25046acc9102f17cd5ccbf285c85d92682282be1
Kyle Lucas <epipkl@gmail.com>
2023-02-28 01:04:17 -0700

    pls commit

---
+++ maps/Generic_Home_1_Layer_1_48x48.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Generic_Home_1_Layer_1_48x48" tilewidth="48" tileheight="48" tilecount="182" columns="14">
+ <image source="../../Modern Pixel Tileset/Modern Interiors/6_Home_Designs/Generic_Home_Designs/48x48/Generic_Home_1_Layer_1_48x48.png" width="672" height="642"/>
+</tileset>
---
+++ maps/KyleSprite.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="KyleSprite" tilewidth="48" tileheight="48" tilecount="2240" columns="56">
+ <image source="../../Modern Pixel Tileset/LimeZu Character Generator Release 1.1.5/Saved Characters/KyleSprite.png" width="2688" height="1920"/>
+</tileset>
